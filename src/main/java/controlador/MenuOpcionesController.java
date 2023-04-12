544ad783ee7eb1b3d9b742036ca2adc3c41292dd
Cristopher0206 <cristopher.perez@epn.edu.ec>
2023-04-11 20:52:29 -0500

    para el Arielo

--- src/main/java/controlador/MenuOpcionesController.java
+++ src/main/java/controlador/MenuOpcionesController.java
@@ -6,28 +6,24 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-@WebServlet("/LoginController")
-public class LoginController extends HttpServlet {
+@WebServlet("/MenuOpcionesController")
+public class MenuOpcionesController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public LoginController() {
+    public MenuOpcionesController() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("jsp/login.jsp");
+		procesarSolicitud(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String password = request.getParameter("password");
-		if (password == "DECAB2022") {
-			// creamos la sesión
-			request.getRequestDispatcher("/MenuOpcionesController").forward(request, response);
-		} else {
-			response.sendRedirect("jsp/login.jsp");
-		}
+		procesarSolicitud(request, response);
 	}
-
+	
+	private void procesarSolicitud(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/jsp/menuopciones.jsp").forward(request, response);
+	}
 }
---
+++ src/main/webapp/jsp/login.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login</title>
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+	crossorigin="anonymous">
+<!-- CSS only -->
+	<link rel="stylesheet" href="/css/estilos.css">
+</head>
+<body>
+	<nav class="navbar navbar-dark bg-dark">
+		<div class="container-fluid">
+			<span class="navbar-brand mb-0 h1">Departamento de Control de Alimentos y Biotecnología</span>
+		</div>
+
+	</nav>
+
+	<div class="wrapper">
+		<div class="formContent">
+			<form method="post" action="../LoginController">
+				<fieldset>
+					<legend>Inicio de Sesión</legend>
+					<p>Clave</p>
+					<input type="password" name="password" /> <br> <br> <br>
+					<input type="submit" value="Ingresr" />
+				</fieldset>
+			</form>
+		</div>
+
+	</div>
+</body>
+</html>
