6a0bbfd05467887c658d06261e9ab3104b3d3ff8
Ariel27R <ariel.rosero@epn.edu.ec>
2023-04-13 08:22:26 -0500

    Modelo Proveedor

--- src/main/java/modelo/Proveedor.java
+++ src/main/java/modelo/Proveedor.java
@@ -2,6 +2,8 @@ package modelo;
 
 import java.io.Serializable;
 
+import modelo.conexion.ConexionBDD;
+
 public class Proveedor implements Serializable {
 	private static final long serialVersionUID = 2L;
 	
@@ -46,5 +48,33 @@ public class Proveedor implements Serializable {
 		this.email = email;
 	}
 	
+	public List<Proveedor> getProveedores(){
+		String sql= "SELECT * FROM PROVEEDOR";
+		Connection cnn = null;
+		ResultSet rs = null;
+		List<Proveedor> proveedores = new ArrayList<Proveedor>();
+		
+		//2.- Conectar a la BDD 
+		cnn = conexion.ConexionBDD.getConexion();
+		try {
+				r0s = cnn.prepareStatement(sql).executeQuery();
+				while(rs.next()) {
+					Proveedor p = new Proveedor(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
+					proveedores.add(p);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}finally {
+				//3.- Cerrar
+				conexion.ConexionBDD.cerrar(rs);
+		}
+		return proveedores;
+	}
 	
+	public boolean create(Proveedor p) {
+		
+		boolean bandera = false;
+		
+		String sql= "INSERT INTO proveedor"
+	}
 }
